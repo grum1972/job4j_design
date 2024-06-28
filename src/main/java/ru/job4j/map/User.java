f3d9178fcb1eada94b688996928c216761748752
Kotov Oleg <grum1972@gmail.com>
2024-06-28 21:41:43 +0300

    8. Коллизии Map

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -1,6 +1,6 @@
 package ru.job4j.map;
 
-import java.util.Calendar;
+import java.util.Objects;
 
 public class User {
     private String name;
@@ -9,5 +9,22 @@ public class User {
     public User(String name, int children) {
         this.name = name;
         this.children = children;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return children == user.children && Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, children);
     }
 }
