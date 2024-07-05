38a33ee15943f209a1e0d55c4859e22302cdae07
Kotov Oleg <grum1972@gmail.com>
2024-07-06 00:04:41 +0300

    12. Валидация параметров запуска

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -9,13 +9,32 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get("./data");
-        search(start, path -> path.toFile().getName().endsWith(".txt")).forEach(System.out::println);
+        if (args.length != 2) {
+            throw new IllegalArgumentException("You need input root folder "
+                    + "and file extension for search");
+        }
+        Path start = Paths.get(args[0]);
+        validate(args);
+        search(start, path -> path.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    public static void validate(String[] args) {
+        if (args.length < 2) {
+            throw new IllegalArgumentException("Your arguments isn't valid");
+        }
+        if (!Files.isDirectory(Path.of(args[0]))) {
+            throw new IllegalArgumentException(args[0] + " isn't directory");
+        }
+        if (!args[1].startsWith(".")) {
+            throw new IllegalArgumentException("The extension of "
+                    + "the file you are searching for should start with \".\"");
+        }
+
     }
 }
